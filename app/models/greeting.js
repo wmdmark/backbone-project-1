4530c5cc44460e849329829096554d6885f792b0
Lindsay <lbull144@students.bju.edu>
2015-09-21 14:52:00 -0400

    Finished backbone project

--- app/models/greeting.js
+++ app/models/greeting.js
@@ -5,5 +5,12 @@ Greeting = Backbone.Model.extend({
   }
 });
 
+//The model that will be used for every item inside the collection
+//(Backbone will traw to map every item into this model)
+AlbumModel = Backbone.Model.extend({
+    url: '/data/album.json'
+  });
+
 // Make the model available to require imports
-module.exports = Greeting
+//module.exports = Greeting
+module.exports = AlbumModel
