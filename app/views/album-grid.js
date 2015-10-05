eb28d76f3a69f7483700b4ce8686aed0141df63f
Lindsay <lbull144@students.bju.edu>
2015-10-05 14:51:06 -0400

    Parse Part 1

--- app/views/album-grid.js
+++ app/views/album-grid.js
@@ -1,26 +1,44 @@
+var Album = Parse.Object.extend("Album");
+var Albume = Backbone.Model.extend({});
+
+var AlbumCollection = Backbone.Collection.extend({
+  model: Albume
+});
+
+var albumCollection = new AlbumCollection();
 
 var AlbumListView = Backbone.View.extend({
-    initialize: function(album){
-      this.listenTo(this.model, 'change', this.render);
+    initialize: function(Album){
+      this.collection = albumCollection;
+      this.listenTo(this.collection, 'add', this.render);
+      if(Album)
+        this.loadAlbums();
     },
+
+    loadAlbums: function() {
+      var query = new Parse.Query(Album)
+      query.find().then(function(results,a,b) {
+        // for each result, render the album-list-view and append to this.$el
+        for (var i = 0; i < results.length; i++) {
+          var object = results[i];
+          albumCollection.add(object.attributes);
+        }
+      })
+    },
+
     render: function() {
-      /*
-      var template = $('#CDTemplate').html();
-      template = Handlebars.compile(template);
+    // Load our template from the templates folder
+      var template = require("./templates/album-grid")
+      var albumList = albumCollection.models;
+      var albums = [];
+      albumList.forEach(function(model){
+        albums.push(model.attributes);
+      })
 
-      this.$el.html(template(this.model.toJSON()));
-      */
-
-      // Load our template from the templates folder
-    var template = require("./templates/album")
-
-    // Render the template into our el using the 
-    // model's JSON data
-
-    this.$el.html(template(this.model.toJSON()))
-      $("#app-views").append(this.$el);
+      this.$el.html(template({albums: albums}));
+      return this;
     }
 });
 
-module.exports = ParseObject 
+module.exports = Album 
 module.exports = AlbumListView
